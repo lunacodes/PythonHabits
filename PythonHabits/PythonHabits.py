1f1727c249314189145b14c6ea654c12e4dba1ca
Luna Lunapiena <lunacodes@gmail.com>
2018-08-01 19:11:46 -0400

    middle of rewrite to use sqlalchemy with flask_sqlalchemy - unclear how to have multiple functioning taxonomies (habits, dailes, etc)

--- PythonHabits/PythonHabits.py
+++ PythonHabits/PythonHabits.py
@@ -1,12 +1,102 @@
-from flask import Flask
+#!/usr/bin/env python
+import os
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+# from tabledef import *
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "user_info.db"))
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+
+db = SQLAlchemy(app)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+class Habit(db.Model):
+    """docstring for Habit"""
+    habit_title = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
 
+    def __repr__(self):
+        return "<Habits: {}".format(self.habit_title)
+
+
+class Daily(db.Model):
+    """docstring for Daily"""
+    daily_title = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
+
+    def __repr__(self):
+        return "<Dailies: {}".format(self.daily_title)
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.form:
+        habit = Habit(habit_title=request.form.get("habit_title"))
+        db.session.add(habit)
+        db.session.commit()
+
+        daily = Daily(daily_title=request.form.get("daily_title"))
+        db.session.add(daily)
+        db.session.commit()
+
+    habits = Habit.query.all()
+    dailies = Daily.query.all()
+    
+    return render_template("home.html", habits=habits, dailies=dailies)
 
 if __name__ == '__main__':
-    app.run()
+    app.secret_key = os.urandom(12)
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
+### Original Code (mostly) ###
+# engine = create_engine('sqlite:///user_info.db', echo=True)
+
+# class Item(db.Model):
+#     """Parent Class for Habits, Dailies, To-Dos and Rewards"""
+
+#     title = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
+
+#     def __repr__(self):
+#         return "<Title: {}>".format(self.title)
+# 
+# 
+# @app.route('/', methods=["GET", "POST"])
+# def home():
+#     if not session.get('logged_in'):
+#         return render_template('main.html')
+#     else:
+#         return "Congrats - you've logged in!! <a href='/logout'>Logout</a>"
+
+#     if request.form:
+#         habit = Habit(title=request.form.get("title"))
+
+#         db.session.add(habit)
+#         db.session.commit(all)
+#         habits = Habit.query.all()
+
+#         return render_template("main.html", habits=habits)
+
+
+# @app.route('/login', methods=["POST"])
+# def do_admin_login():
+
+#     POST_USERNAME = str(request.form['username'])
+#     POST_PASSWORD = str(request.form['password'])
+
+#     Session = sessionmaker(bind=engine)
+#     s = Session()
+#     query = s.query(User).filter(User.username.in_(
+#         [POST_USERNAME]), User.password.in_([POST_PASSWORD]))
+#     result = query.first()
+
+#     if result:
+#         session['logged_in'] = True
+#     else:
+#         flash('please enter the correct user info.')
+
+#     return home()
+
+# if __name__ == '__main__':
+#     app.secret_key = os.urandom(12)
+#     app.run(debug=True, host='0.0.0.0', port=5000)
--- readme.md
+++ readme.md
@@ -1,3 +1,19 @@
 # Python Habits
 
-A lo-fi imitation of the Habitica app (formerly known as HabitRPG, for learning purposes using Flask.
+A lo-fi imitation of the Habitica app (formerly known as HabitRPG), for learning purposes using Flask.
+
+## Next Steps
+
+### Database
+
+* Replace `sqlalchemy` with `flask_sqlalchemy` 
+* Need classes for Users (stats, password, etc) and task categories (habits, dailies, to-dos, rewards)
+
+### Login Function
+
+* Requires: Create Database w/ structure from dummy file and tabledef
+
+### Registration Page
+
+* Requires: Mechanism to add info to the database
+
